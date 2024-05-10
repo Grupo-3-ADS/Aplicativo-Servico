287c7b661811d00b3da255988ee801cb22415ca2
yNanneRy <renangealves@gmail.com>
2024-05-09 21:53:52 -0300

    Modelagem do objeto feita

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lista de Tarefas',
+      title: 'Serviços',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
@@ -20,8 +20,8 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Lista de Tarefas'),
       routes: {
-        '/register': (context) => const RegisterTask(),
-        '/list': (context) => const TaskList(),
+        '/register': (context) => const RegisterService(),
+        '/list': (context) => const ServiceList(),
       },
     );
   }
